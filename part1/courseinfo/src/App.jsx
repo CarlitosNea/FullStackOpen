5486637000dff1a0dd801a93c11d46cca5dae741
Carlitos Programador <125935411+CarlitosNea@users.noreply.github.com>
2024-10-23 00:58:38 -0500

    Courseinfo step 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,23 +1,39 @@
 // definicion del componente header
 const Header = (props) => {
+  console.log("Header")
   console.log(props)
   return (
     <h1>{props.course_name}</h1>
   )
 }
 
+// definicion del componente part
+const Part = (props) =>{
+  console.log("Part")
+  console.log(props)
+  return (
+    <p>
+      {props.part_name} {props.part_exercises}
+    </p>
+  )
+}
+
 // definicion del componente content
 const Content = (props) => {
+  console.log("Content")
   console.log(props)
-  return(
-    <p>
-      {props.course_part} {props.part_exercises}
-    </p>
+  return (
+    <div>
+      <Part part_name={props.part1} part_exercises={props.exercises1} />
+      <Part part_name={props.part2} part_exercises={props.exercises2} />
+      <Part part_name={props.part3} part_exercises={props.exercises3} />
+    </div>
   )
 }
 
 // definicion del componente total
 const Total = (props) => {
+  console.log("Total")
   console.log(props)
   return (
     <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
@@ -38,10 +54,8 @@ const App = () => {
       {/* Llamado del Header y su contenido "course" */}
       <Header course_name={course} />
 
-      {/* Llamado del Content y sus contenidos "part#" y "exercises#" */}
-      <Content course_part={part1} part_exercises={exercises1}/>
-      <Content course_part={part2} part_exercises={exercises2}/>
-      <Content course_part={part3} part_exercises={exercises3}/>
+      {/* Llamado del Content */}
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
 
       {/* Llamado del Total y sus valores */}
       <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
